a4012a5e26e565021623892103212415f3ef438b
Dallin Breen <dallinbreen624@gmail.com>
2022-04-01 11:34:09 -0600

    Added functionality to gather user data and display info on button clicks

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,24 +1,79 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../App";
 import "./Display.css";
 
 export default function Display() {
   const { userData, setUserData } = useContext(DataContext);
+  const [index, setIndex] = useState(1);
 
-  function clickHandler() {
-    console.log(userData);
+  console.log(userData);
+
+  function prevHandler() {
+    if (index > 1) {
+      setIndex(index - 1);
+    } else {
+      console.log("cant go down more");
+    }
   }
+
+  function nextHandler() {
+    if (index < userData.length) {
+      setIndex(index + 1);
+    } else {
+      console.log("cant go up more");
+    }
+  }
+
   return (
     <div className="Display">
-      <div className="box"></div>
+      <div className="box">
+        <h2 className="counter">
+          {index}/{userData.length}
+        </h2>
+        {userData
+          .filter((person) => {
+            return person.id === index;
+          })
+          .map((person, index) => {
+            return (
+              <div key={index} className="user-display">
+                <h2 className="name">
+                  {person.name.first} {person.name.last}
+                </h2>
+                <div className="user-info">
+                  <h3 className="info">
+                    From:{" "}
+                    <p>
+                      {person.city}, {person.country}
+                    </p>
+                  </h3>
+                  <h3 className="info">
+                    Job Title: <p>{person.title}</p>
+                  </h3>
+                  <h3 className="info">
+                    Employer: <p>{person.employer}</p>
+                  </h3>
+                </div>
+                <h3>Favorite Movies:</h3>
+                <ol>
+                  <li>{person.favoriteMovies[0]}</li>
+                  <li>{person.favoriteMovies[1]}</li>
+                  <li>{person.favoriteMovies[2]}</li>
+                </ol>
+              </div>
+            );
+          })}
+      </div>
       <div className="nav-bar">
-        <button className="direction-btns">{"< Previous"}</button>
+        <button className="direction-btns" onClick={prevHandler}>
+          {"< Previous"}
+        </button>
         <div className="middle-btns">
           <button>{"Edit"}</button>
           <button>{"Delete"}</button>
           <button>{"New"}</button>
         </div>
-        <button className="direction-btns" onClick={clickHandler}>
+        <button className="direction-btns" onClick={nextHandler}>
           {"Next >"}
         </button>
       </div>
